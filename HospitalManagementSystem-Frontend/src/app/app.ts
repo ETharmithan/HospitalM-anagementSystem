6db322eb1c2d7c5beee2bcbdebca1ad571904e3f
sanjeevan2001 <2001sanjeevan@gmail.com>
2025-10-31 04:39:37 +0530

    frontend-setup

---
+++ HospitalManagementSystem-Frontend/src/app/app.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  private http = inject(HttpClient);
+  protected readonly title = signal('HospitalManagementSystem-Frontend');
+
+}
