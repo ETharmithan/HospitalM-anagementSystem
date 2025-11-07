46dcbb8808569a5559c242d46ff35ba99ca60b0e
sanjeevan2001 <2001sanjeevan@gmail.com>
2025-11-07 06:01:55 +0530

    add dto

--- HospitalManagementSystem.Domain/Models/Patient.cs
+++ HospitalManagementSystem.Domain/Models/Patient.cs
@@ -1,7 +1,11 @@
-﻿namespace HospitalManagementSystem.Domain.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalManagementSystem.Domain.Models
 {
     public class Patient
     {
+        [Key]
         public Guid PatientId { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
@@ -12,6 +16,9 @@
         public Patient_Medical_History? MedicalHistory { get; set; }
         public Patient_Medical_Related_Info? MedicalRelatedInfo { get; set; }
         public Patient_Emergency_Contact? EmergencyContact { get; set; }
-        public Patient_Login_Info? LoginInfo { get; set; }
+
+
+   
+        public Patient_Login_Info LoginInfo { get; set; } = null!;
     }
 }
