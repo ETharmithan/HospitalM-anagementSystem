fcc13169b69e32897e4592013d6a1b4b68c39c6c
sanjeevan2001 <2001sanjeevan@gmail.com>
2025-11-09 23:25:04 +0530

    small change

---
+++ HospitalManagementSystem-Frontend/src/types/user.ts
@@ -0,0 +1,15 @@
+export type User = {
+  id: string;
+  displayName: string;
+  email: string;
+  token: string;
+  imageUrl?: string;
+};
+
+
+
+export type RegisterCreds = {
+  email: string;
+  displayName: string;
+  password: string;
+};
