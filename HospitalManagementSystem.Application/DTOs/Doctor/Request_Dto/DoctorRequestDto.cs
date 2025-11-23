62ec7007a09711e104089943e4c6c2440bbc3343
sanjeevan2001 <2001sanjeevan@gmail.com>
2025-11-23 14:20:59 +0530

    Doctor related DTOs

---
+++ HospitalManagementSystem.Application/DTOs/Doctor/Request_Dto/DoctorRequestDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Application.DTOs.Doctor.Request_Dto
+{
+    public class DoctorRequestDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        public string Phone { get; set; } = null!;
+
+
+        [Required(ErrorMessage = "License number is required")]
+        [MaxLength(50)]
+        public string LicenseNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = "Qualification is required")]
+        public string Qualification { get; set; } = null!;
+
+        [Required(ErrorMessage = "Status is required")]
+        [MaxLength(50)]
+        public string Status { get; set; } = null!;
+
+        //ForeignKey
+        [Required(ErrorMessage = "DepartmentId is required")]
+        public Guid DepartmentId { get; set; }
+
+    }
+}
