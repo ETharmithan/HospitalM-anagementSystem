380a4cef938cfe6f4f904d474143921c124f4f01
sanjeevan2001 <2001sanjeevan@gmail.com>
2025-11-23 14:23:06 +0530

    Doctor repository interfaces

---
+++ HospitalManagementSystem.Domain/IRepository/IDoctorLeaveRepository.cs
@@ -0,0 +1,19 @@
+﻿using HospitalManagementSystem.Domain.Models.Doctor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Domain.IRepository
+{
+    public interface IDoctorLeaveRepository
+    {
+        Task<IEnumerable<DoctorLeave>> GetAllAsync();
+        Task<DoctorLeave?> GetByIdAsync(Guid id);
+        Task<DoctorLeave> CreateAsync(DoctorLeave doctorLeave);
+        Task<DoctorLeave?> UpdateAsync(DoctorLeave doctorLeave);
+        Task<bool> DeleteAsync(Guid id);
+
+    }
+}
---
+++ HospitalManagementSystem.Domain/IRepository/IDoctorScheduleRepository.cs
@@ -0,0 +1,18 @@
+﻿using HospitalManagementSystem.Domain.Models.Doctor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Domain.IRepository
+{
+    public interface IDoctorScheduleRepository
+    {
+        Task<IEnumerable<DoctorSchedule>> GetAllAsync();
+        Task<DoctorSchedule?> GetByIdAsync(Guid id);
+        Task<DoctorSchedule> CreateAsync(DoctorSchedule doctorSchedule);
+        Task<bool> DeleteAsync(Guid id);
+
+    }
+}
