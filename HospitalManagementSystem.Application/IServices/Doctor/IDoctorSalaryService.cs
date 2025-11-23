80cb35efb947ac1043786728a84c58124926a691
sanjeevan2001 <2001sanjeevan@gmail.com>
2025-11-23 14:22:15 +0530

    Doctor Interfaces

---
+++ HospitalManagementSystem.Application/IServices/Doctor/IDoctorSalaryService.cs
@@ -0,0 +1,19 @@
+﻿using HospitalManagementSystem.Application.DTOs.Doctor.Request_Dto;
+using HospitalManagementSystem.Application.DTOs.Doctor.Response_Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Application.IServices.Doctor
+{
+    internal interface IDoctorSalaryService
+    {
+        Task<IEnumerable<DoctorSalaryResponseDto>> GetAllAsync();
+        Task<DoctorSalaryResponseDto?> GetByIdAsync(Guid id);
+        Task<DoctorSalaryResponseDto> CreateAsync(DoctorSalaryRequestDto doctorSalaryRequestDto);
+        Task<bool> DeleteAsync(Guid id);
+
+    }
+}
