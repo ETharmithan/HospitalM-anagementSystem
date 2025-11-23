c2544c544681c03902c1a0ce7f3b3dca7511cdd4
sanjeevan2001 <2001sanjeevan@gmail.com>
2025-11-23 14:24:49 +0530

    Doctor Repository

--- HospitalManagementSystem.Infrastructure/Data/AppDbContext.cs
+++ HospitalManagementSystem.Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HospitalManagementSystem.Domain.Models.Doctor;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,54 @@ namespace HospitalManagementSystem.Infrastructure.Data
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
+        public DbSet<Department> Departments { get; set; } = null!;
+        public DbSet<Doctor> Doctors { get; set; } = null!;
+        public DbSet<DoctorAppointment> DoctorAppointments { get; set; } = null!;
+        public DbSet<DoctorLeave> DoctorLeaves { get; set; } = null!;
+        public DbSet<DoctorPatientRecords> DoctorPatientRecords { get; set; } = null!;
+        public DbSet<DoctorSalary> DoctorSalaries { get; set; } = null!;
+        public DbSet<DoctorSchedule> DoctorSchedules { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Department>()
+            .HasMany(d => d.Doctors)
+            .WithOne(d => d.Department)
+            .HasForeignKey(d => d.DepartmentId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Doctor>()
+            .HasMany(doctor => doctor.DoctorAppointments)
+            .WithOne(doctor => doctor.Doctor)
+            .HasForeignKey(doctor => doctor.DoctorId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Doctor>()
+            .HasMany(doctor => doctor.DoctorLeaves)
+            .WithOne(doctor => doctor.Doctor)
+            .HasForeignKey(doctor => doctor.DoctorId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Doctor>()
+            .HasMany(doctor => doctor.DoctorPatientRecords)
+            .WithOne(doctor => doctor.Doctor)
+            .HasForeignKey(doctor => doctor.DoctorId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Doctor>()
+            .HasMany(doctor => doctor.DoctorSalaries)
+            .WithOne(doctor => doctor.Doctor)
+            .HasForeignKey(doctor => doctor.DoctorId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Doctor>()
+            .HasMany(doctor => doctor.DoctorSchedules)
+            .WithOne(doctor => doctor.Doctor)
+            .HasForeignKey(doctor => doctor.DoctorId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        }
     }
 }
---
+++ HospitalManagementSystem.Infrastructure/Repository/Doctor/DepartmentRepository.cs
@@ -0,0 +1,47 @@
+﻿using HospitalManagementSystem.Domain.IRepository;
+using HospitalManagementSystem.Domain.Models.Doctor;
+using HospitalManagementSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Infrastructure.Repository.Doctor
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DepartmentRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IEnumerable<Department>> GetAllAsync()
+            => await _appDbContext.Departments.ToListAsync();
+
+        public async Task<Department?> GetByIdAsync(Guid id)
+            => await _appDbContext.Departments.FindAsync(id);
+
+        public async Task<Department> CreateAsync(Department department)
+        {
+            _appDbContext.Departments.Add(department);
+            await _appDbContext.SaveChangesAsync();
+            return department;
+        }
+
+        public async Task UpdateAsync(Department department)
+        {
+            _appDbContext.Departments.Update(department);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(Department department)
+        {
+            _appDbContext.Departments.Remove(department);
+            return _appDbContext.SaveChangesAsync();
+        }
+    }
+}
